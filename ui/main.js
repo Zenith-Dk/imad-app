b062b2b2694b75eebbeb538f5f8acdc27fcc996d
Zenith-Dk <2045online@gmail.com>
2017-09-02 22:40:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,11 @@ console.log('Loaded!');
 //change the element
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
 // move the image
 var img = document.getElementById('img1');
 img.onclick = function(){
-    img.style.marginleft = '100px';
+    img.style.marginLeft = '100px';
 };
     
 
