d1f5f36fb3cda87fbb9de0bbc1fa6e73719c5709
Zenith-Dk <2045online@gmail.com>
2017-09-05 16:10:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,19 @@
-console.log('Loaded!');
-//change the element
-var element = document.getElementById('main-text');
-element.innerHTML = 'new value';
-
-// move the image
-var img = document.getElementById('img1');
-var marginLeft = 0;
-var moveRight=function(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft= marginLeft + 'px'; 
-};
-img.onclick = function(){
-    var interval = setInterval(moveRight, 40);
-    
+// counter 
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+  //make a request to the counter endpoint
+  
+  
+  // capture the response and store it in a variable
+  
+  
+  
+  // Render the variable in the correct span
+   counter = counter + 1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
+  
 };
  
 
